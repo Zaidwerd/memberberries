104da11fc3fff9befa59524f703db26565ac6436
Paris Leach <parisleach@gmail.com>
2016-11-21 12:27:15 -0500

    changed everything

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-document.onload() => {
-  document.body.style.background = "url('http://24.media.tumblr.com/808b8030b0b22576b0eec4edd50d3cb5/tumblr_mr6sl44Cya1qam8b5o1_250.gif')";
-}();
+document.body.style.background = `url('http://24.media.tumblr.com/808b8030b0b22576b0eec4edd50d3cb5/tumblr_mr6sl44Cya1qam8b5o1_250.gif')`;
